01d3b83acc13e982133b495d912e6c50eddf488c
Alexander Yaroshevich <vozman@yandex.ru>
2017-12-29 17:08:40 +0300

    change setup py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import slide_analysis_service
 with open('requirements.txt') as file:
     INSTALL_REQUIRES = [l.strip() for l in file.readlines() if l]
 
-setuptools.setup(name='slide_analysis',
+setuptools.setup(name='slide_analysis_service',
                  version=slide_analysis_service.__version__,
                  url='https://github.com/Vozf/slide-analysis-service/',
                  license='???',  # todo: add
