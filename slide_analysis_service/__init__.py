e7883d587697fbedfe4a66b42bca3cb1ae6f7eb3
Alexander Yaroshevich <vozman@yandex.ru>
2018-05-13 22:56:05 +0300

    fix init

--- slide_analysis_service/__init__.py
+++ slide_analysis_service/__init__.py
@@ -11,13 +11,12 @@ if __name__ == '__main__':
     print(slideAnalysisService.get_descriptors())
     print(slideAnalysisService.get_similarities())
     print(slideAnalysisService.get_directory())
-    slide = slideAnalysisService.get_slide(default_img,
-                                           slideAnalysisService.get_descriptors()[1]())
+    slide = slideAnalysisService.get_slide(default_img, 'histogram')
     print(slide)
     print(slide.is_ready())
     if not slide.is_ready():
         slide.precalculate()
-    similar = slide.find((0, 0, 256, 256), slideAnalysisService.get_similarities()[1]())
+    similar = slide.find((0, 0, 256, 256), 'euclidean')
     print(similar)
 
     print('All done')
--- slide_analysis_service/descriptors/__init__.py
+++ slide_analysis_service/descriptors/__init__.py
@@ -3,10 +3,11 @@ from collections import OrderedDict
 from slide_analysis_service.descriptors.histogram_descriptor_class import HistogramDescriptor
 from slide_analysis_service.descriptors.test_descriptor_class import TestDescriptor
 from slide_analysis_service.descriptors.co_occurrence_descriptor_class import CoOccurrenceDescriptor
+from slide_analysis_service.descriptors.constants import COLOR_RANGE
+
 all_descriptors = [TestDescriptor, HistogramDescriptor, CoOccurrenceDescriptor]
 all_descriptors_dict = OrderedDict([
     ("histogram", HistogramDescriptor),
     ("cooccurence", CoOccurrenceDescriptor),
 ])
 
-from slide_analysis_service.descriptors.constants import COLOR_RANGE
