59d0bd700d5ffd0b457910a3d048904af1e5615e
Alexander Yaroshevich <vozman@yandex.ru>
2018-10-20 23:40:08 +0300

    add missing req to setup py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=['matplotlib',
                       'numpy',
                       'scikit-image',
+                      'scipy',
                       ],
     url='',
     license='MIT',
