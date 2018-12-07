60aee4c61992776a98b9e2a9fa5aa3444de0a802
Alexander Yaroshevich <vozman@yandex.ru>
2018-12-07 19:53:33 +0300

    fix no lut in build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='slide_analysis_service',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     install_requires=['matplotlib',
                       'numpy',
