41686fa95557912535bef862755b566c6b05f747
Alexander Yaroshevich <a_yaroshevich@indatalabs.com>
2019-04-29 15:45:22 +0300

    Refactor recalculate

--- slide_analysis_service/precalculate.py
+++ slide_analysis_service/precalculate.py
@@ -4,22 +4,29 @@
 import glob
 import sys
 from pathlib import Path
+from openslide import OpenSlide
+from tqdm import tqdm
 
 from slide_analysis_service import SlideAnalysisService
 
-if len(sys.argv) != 2:
-    raise RuntimeError('Number of arguments should be one. Please pass path to slides folder')
 
-path = sys.argv[1]
+def precalculate(path, recursive=False):
+    saserv = SlideAnalysisService()
+    img_paths = glob.glob(str(Path(path) / '**' / '*.*'), recursive=recursive)
+    print(f'Found total {len(img_paths)} files'
+          f' and {len(SlideAnalysisService.get_descriptors().keys())} descriptors')
+    for path in tqdm(img_paths):
+        if not OpenSlide.detect_format(path):
+            return
 
-saserv = SlideAnalysisService()
-images = glob.glob(str(Path(path) / '**' / '*.tif'), recursive=True)
-print(f'Found total {len(images)} images'
-      f' and {len(SlideAnalysisService.get_descriptors().keys())} descriptors')
-for img in images:
-    for descriptor in SlideAnalysisService.get_descriptors().keys():
-        slide = saserv.get_slide(img, descriptor)
-        print(img)
-        if not slide.is_ready():
-            slide.precalculate()
+        for descriptor in SlideAnalysisService.get_descriptors().keys():
+            slide = saserv.get_slide(path, descriptor)
+            if not slide.is_ready():
+                slide.precalculate()
 
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise RuntimeError('Number of arguments should be one. Please pass path to slides folder')
+    path = sys.argv[1]
+    precalculate(path, recursive=True)
